8d25688b2c1ebdef08be10a16e51d649b9f119aa
majkel <kamil.machura1@gmail.com>
2019-11-25 18:51:18 +0100

    dodanie pól do obiektu zadań

--- src/main/java/pl/springnauka/Tasks/Task.java
+++ src/main/java/pl/springnauka/Tasks/Task.java
@@ -2,8 +2,12 @@ package pl.springnauka.Tasks;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class Task {
 
     private String title;
+    private String author;
+    private LocalDate date;
 }
