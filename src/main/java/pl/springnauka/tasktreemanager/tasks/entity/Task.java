8649133aa36e211a4855fedb4af7da5537c086e7
majkel <kamil.machura1@gmail.com>
2020-01-11 22:48:22 +0100

    poprawa frontu

--- src/main/java/pl/springnauka/tasktreemanager/tasks/entity/Task.java
+++ src/main/java/pl/springnauka/tasktreemanager/tasks/entity/Task.java
@@ -8,11 +8,16 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
-public class Task {
+public class Task implements Comparable<Task> {
 
     private Long id;
     private String title;
     private String description;
     private LocalDateTime createdAt;
     private List<String> files;
+
+    @Override
+    public int compareTo(Task o) {
+        return this.getId().compareTo(o.getId());
+    }
 }
